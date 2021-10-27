e93e0dd0bf0927af5961b76b170ff34b91ae02f8
fdiemon812 <flavio.dediego@gmail.com>
2021-10-27 10:22:39 +0200

    Prueba

--- apiWebStorage/script.js
+++ apiWebStorage/script.js
@@ -7,15 +7,6 @@
 document.getElementById("enviar").addEventListener("click", addTarea);
 
 
-
-
-// sessionStorage.setItem("contador", "1")
-// let contadorNuevo= ""+(parseInt(sessionStorage.getItem("contador"))+1)
-// sessionStorage.setItem("contador", contadorNuevo);
-
-// console.log(contadorNuevo)
-
-let listaTareas=[sessionStorage.getItem("tareas")]
 function addTarea(ev){
 
     ev.preventDefault()
@@ -43,10 +34,5 @@ function addTarea(ev){
 
 document.getElementById("reset").addEventListener("click", ()=>{
 
-
-
     sessionStorage.clear()
-    console.log("elementos borrados")
-
-
 });
