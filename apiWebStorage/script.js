fea35a536b0d0e0bb0748898694c036efd6053d1
fdiemon812 <flavio.dediego@gmail.com>
2021-10-27 14:27:05 +0200

    Resolviendo refresco tachado y marcado

--- apiWebStorage/script.js
+++ apiWebStorage/script.js
@@ -2,8 +2,73 @@
 
 
 
+    document.getElementById("tabla").addEventListener("click", pulsandoTabla)
 
-//ADD TAREA AL SESSION
+    function pulsandoTabla(ev){
+
+
+        if(ev.target.matches(".boton")){
+            
+            let claveTareaCheck= ev.target.id
+            localStorage.removeItem(claveTareaCheck);
+            location.reload()
+
+        }else if(ev.target.matches(".check")){
+
+            if(ev.target.checked==true){
+
+                let tareaJson= JSON.parse(localStorage.getItem(ev.target.id))
+                tareaJson.estado="true"
+                localStorage.setItem(ev.target.id, JSON.stringify(tareaJson))
+                
+
+            }else{
+
+                let tareaJson= JSON.parse(localStorage.getItem(ev.target.id))
+                tareaJson.estado="false"
+                localStorage.setItem(ev.target.id, JSON.stringify(tareaJson))
+
+            }
+
+
+        }
+
+    }
+
+    Object.keys(localStorage).forEach(function(key){
+
+
+        let task = JSON.parse(localStorage.getItem(key))
+        let tabla= document.getElementById("tabla")
+
+        // let filaNueva="";
+
+        // if(task.estado){
+
+
+        //      filaNueva= "<tr><td><input type='checkbox' checked='true' class='check' id='"+key+"'>"+
+        //     task.descripcion+"</td><td><button class='boton' id='"+key+"'>Borrar</button></td></tr>"
+
+        // }else{
+
+        //      filaNueva= "<tr><td><input type='checkbox' checked='false' class='check' id='"+key+"'>"+
+        //     task.descripcion+"</td><td><button class='boton' id='"+key+"'>Borrar</button></td></tr>"
+        // }
+        
+        
+        
+       tabla.insertRow().innerHTML = filaNueva;               
+        
+    });
+ 
+
+
+
+    
+ 
+
+
+//ADD TAREA AL LOCAL
 document.getElementById("enviar").addEventListener("click", addTarea);
 
 
@@ -20,19 +85,18 @@ function addTarea(ev){
 
     }
 
+    let clave = localStorage.length+1
+    localStorage.setItem(clave, JSON.stringify(miTarea));
+
+    location.reload();
     
-    listaTareas.push(miTarea)
 
-
-    sessionStorage.setItem("tareas", listaTareas);
-
-    console.log(listaTareas)
 }
 
 
-//Borrar memoria sesion
+//Borrar memoria local
 
 document.getElementById("reset").addEventListener("click", ()=>{
 
-    sessionStorage.clear()
+    localStorage.clear()
 });
